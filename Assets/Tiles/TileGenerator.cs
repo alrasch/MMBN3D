ab79a5efe090130901de3314c0502c2ec1c7a91b
alrasch <arasch855@gmail.com>
2021-06-19 01:28:02 +0200

    Add generator for basic tiles of Den Area 1.

---
+++ Assets/Tiles/TileGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace Tiles
+{
+    public class TileGenerator : MonoBehaviour
+    {
+        public GameObject frame;
+        public GameObject tile;
+        void Start()
+        {
+            Tuple<int, int, int>[] tilePositions =
+            {
+                Tuple.Create(0, 0, 0),
+                Tuple.Create(1, 0, 0),
+                Tuple.Create(2, 0, 0),
+                Tuple.Create(3, 0, 0),
+                Tuple.Create(4, 0, 0),
+                Tuple.Create(5, 0, 0),
+                Tuple.Create(6, 0, 0),
+                Tuple.Create(0, 0, 1),
+                Tuple.Create(1, 0, 1),
+                Tuple.Create(2, 0, 1),
+                Tuple.Create(3, 0, 1),
+                Tuple.Create(4, 0, 1),
+                Tuple.Create(5, 0, 1),
+                Tuple.Create(6, 0, 1),
+            };
+            foreach (var (x, y, z) in tilePositions)
+            {
+                Instantiate(frame, new Vector3(x, y, z),
+                    Quaternion.identity);
+                Instantiate(tile, new Vector3(x, y, z),
+                    Quaternion.identity);
+            }
+        }
+    }
+}
